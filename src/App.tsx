2f6a5119241824df1e1f5649a5f6c880685b9733
Benjamin Heller <benjamin.heller@mein.gmx>
2024-04-17 19:42:34 +0200

    call encore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [json, setJson] = useState<{ message: "" }>({ message: "" })
-  useEffect(() => {
-    fetch('https://hista-api-jixu.vercel.app/api/users').then(resp => {
-      resp.json().then(k => { setJson(k) }
-      )
-    }
-    )
-  }, [])
+  const [json, setJson] = useState<{ Resp: "" }>({ Resp: "" })
+
+
+  const onClick = async () => {
+    const resp = await fetch('https://staging-hista-api-dpc2.encr.app/test')
+    const j = await resp.json()
+    setJson(j)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        {json["message"]}
-      </p>
-    </>
+    <div>
+      <button onClick={onClick}>Ruf die API!</button>
+      <h2>{json["Resp"]}</h2>
+    </div>
   )
 }
 
