a8caac1920bf1004fc7edad29c44da91b8401c00
Benjamin Heller <benjamin.heller@mein.gmx>
2024-05-01 09:32:19 +0200

    feat: Clear food input after change

--- src/scenes/Meal/components/AddFood.tsx
+++ src/scenes/Meal/components/AddFood.tsx
@@ -1,12 +1,14 @@
-import { Autocomplete, AutocompleteChangeReason, TextField } from "@mui/material";
+import { Autocomplete, AutocompleteChangeReason, AutocompleteInputChangeReason, TextField } from "@mui/material";
 import useHista from "../../../store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AddFood() {
     const getIngredients = useHista(state => state.getIngredients)
     const postFood = useHista(state => state.postFood)
     const ingredients = useHista(state => state.ingredients)
     const options = ingredients.map(ing => ing.name)
+
+    const [value, setValue] = useState("")
 
 
     useEffect(() => {
@@ -18,7 +20,13 @@ export default function AddFood() {
     const onChange = async (_: React.SyntheticEvent, v: (string | null), changeReason: AutocompleteChangeReason) => {
         if (typeof (v) == 'string' && (changeReason == 'createOption' || changeReason == 'selectOption')) {
             await postFood(v)
+            setValue("")
         }
+    }
+
+    const onInputChange = (_: React.SyntheticEvent, v: string, reason: AutocompleteInputChangeReason) => {
+        console.log(v, reason)
+        setValue(v)
     }
 
 
@@ -27,14 +35,14 @@ export default function AddFood() {
             sx={{ mt: '1rem' }}
             options={options}
             freeSolo
+            inputValue={value}
+            onInputChange={onInputChange}
             onChange={onChange}
             renderInput={params => (
                 <TextField
                     {...params} label={"Zutaten"}
                 />
             )}
-
-
         />
     )
 }
