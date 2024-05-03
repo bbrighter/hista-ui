4359e9beaf659012ab920077dc99ccb9131e7687
Benjamin Heller <benjamin.heller@mein.gmx>
2024-05-03 19:36:58 +0200

    fix: Hinzufügen selber Zutat hintereinander nicht möglich #15

--- src/scenes/Meal/components/AddFood.tsx
+++ src/scenes/Meal/components/AddFood.tsx
@@ -8,33 +8,40 @@ export default function AddFood() {
     const ingredients = useHista(state => state.ingredients)
     const options = ingredients.map(ing => ing.name)
 
-    const [value, setValue] = useState("")
+    const [inputValue, setInputValue] = useState("")
+    const [value, setValue] = useState<string | null>(null)
 
 
     useEffect(() => {
         getIngredients()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [getIngredients])
+
+    useEffect(() => {
+        if (value != null) {
+            postFood(value).then(() => {
+                setValue(null)
+                setInputValue("")
+            }
+            )
+        }
+    }, [postFood, value])
 
 
-    const onChange = async (_: React.SyntheticEvent, v: (string | null), changeReason: AutocompleteChangeReason) => {
-        if (typeof (v) == 'string' && (changeReason == 'createOption' || changeReason == 'selectOption')) {
-            await postFood(v)
-            setValue("")
+    const onChange = async (_e: React.SyntheticEvent, _v: (string | null), changeReason: AutocompleteChangeReason) => {
+        if (typeof (inputValue) == 'string' && (changeReason == 'createOption' || changeReason == 'selectOption')) {
+            setValue(inputValue)
         }
     }
 
-    const onInputChange = (_e: React.SyntheticEvent, v: string) => {
-        setValue(v)
-    }
-
+    const onInputChange = (_e: React.SyntheticEvent, v: string) => { setInputValue(v) }
 
     return (
         <Autocomplete
             sx={{ mt: '1rem' }}
             options={options}
             freeSolo
-            inputValue={value}
+            inputValue={inputValue}
+            value={value}
             onInputChange={onInputChange}
             onChange={onChange}
             renderInput={params => (
