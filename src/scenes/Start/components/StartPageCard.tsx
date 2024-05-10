e4dc1022cf94c516696cd43ae5238080f15d05d6
Benjamin Heller <benjamin.heller@mein.gmx>
2024-05-10 20:06:21 +0200

    fix: css improvements

--- src/scenes/Start/components/StartPageCard.tsx
+++ src/scenes/Start/components/StartPageCard.tsx
@@ -30,9 +30,12 @@ export default function StartPageCard(props: {
     }
 
     return (
-        <Grid item padding={5}>
+        <Grid
+            item
+            sx={{ width: '50%', minWidth: '350px' }}
+        >
             <StyledCard
-                sx={{ minWidth: '300px' }}
+                sx={{ width: '100%' }}
                 variant="outlined"
                 onClick={() => navigate(navigateTo)}
             >
