89e0be5a81da8de27e9d1ed18bad0695d18e4b2b
Benjamin Heller <benjamin.heller@mein.gmx>
2024-05-25 11:55:57 +0200

    feat: Add colors to statistics

--- src/scenes/statistics/components/SymptomEvaluation.tsx
+++ src/scenes/statistics/components/SymptomEvaluation.tsx
@@ -13,6 +13,9 @@ export default function SymptomEvaluation() {
         }
     }, [getIngredients, ingredients.length])
 
+
+    const maxValue = Math.max(...statistics.map(s => s.within72hours))
+
     return (
         <Grid container rowGap={2} sx={{ mt: 2 }}>
             <Grid container spacing={2}>
@@ -28,9 +31,9 @@ export default function SymptomEvaluation() {
                         header
                         subLine={range.foodCondition}
                     />
-                    <KPIPanel value={range.within1hour} />
-                    <KPIPanel value={range.within24hours} />
-                    <KPIPanel value={range.within72hours} />
+                    <KPIPanel value={{ current: range.within1hour, max: maxValue }} />
+                    <KPIPanel value={{ current: range.within24hours, max: maxValue }} />
+                    <KPIPanel value={{ current: range.within72hours, max: maxValue }} />
                 </Grid>
 
             ))}
@@ -39,19 +42,48 @@ export default function SymptomEvaluation() {
 }
 
 function KPIPanel(props: {
-    value?: number
+    value?: { current: number, max: number }
     label?: string
     subLine?: string
     header?: boolean
 }) {
     const elevation = props.header ? 1 : 10
 
+    let backgroundColor: string | undefined = undefined
+    let textcolor: string | undefined = undefined
+    if (props.value != undefined) {
+        const currentValue = props.value.current
+        const maxValue = props.value.max
+        const step = maxValue / 5
+
+        if (currentValue == 0) {
+            backgroundColor = undefined
+        } else if (currentValue < step) {
+            backgroundColor = '#007f4e'
+            textcolor = 'black'
+        } else if (currentValue < step * 2) {
+            backgroundColor = '#72b043'
+            textcolor = 'black'
+        } else if (currentValue < step * 3) {
+            backgroundColor = '#f8cc1b'
+            textcolor = 'black'
+        } else if (currentValue < step * 4) {
+            backgroundColor = '#f37324'
+            textcolor = 'black'
+        } else {
+            backgroundColor = '#e12729'
+            textcolor = 'black'
+        }
+    }
+
+
     return (
         <Grid item xs={3}>
             <Paper
                 elevation={elevation}
-                sx={{ p: 1, textAlign: 'center', minHeight: '70px', alignContent: 'center' }}>
-                <Typography>{props.value}</Typography>
+                sx={{ p: 0, textAlign: 'center', minHeight: '70px', alignContent: 'center', backgroundColor: backgroundColor }}
+            >
+                <Typography color={textcolor}>{props.value?.current}</Typography>
                 <Typography variant='h6'>{props.label}</Typography>
                 <Typography variant="caption">{props.subLine}</Typography>
             </Paper >
