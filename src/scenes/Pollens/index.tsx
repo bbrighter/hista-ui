cac75d7559c69b3fb0195964a033790e555b1434
Benjamin Heller <benjamin.heller@mein.gmx>
2024-06-09 17:09:23 +0200

    feat: improve pollen display

--- src/scenes/Pollens/index.tsx
+++ src/scenes/Pollens/index.tsx
@@ -1,11 +1,11 @@
 import { url } from "../../constants"
-import Pollens from "./Pollens"
+import Pollensview from "./Pollens"
 
 
 const ind = {
     routeProps: {
         path: url.POLLENS,
-        element: <Pollens />,
+        element: <Pollensview />,
     },
     name: "Pollens"
 }
