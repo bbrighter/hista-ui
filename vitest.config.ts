532fa91e20ce52b294f482e0a43ff931db3aec32
Benjamin Heller <benjamin.heller@mein.gmx>
2025-05-01 16:17:49 +0200

    ref: fix knip

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: './setupTest.js',
+        setupFiles: './setupTest.ts',
     },
 })
