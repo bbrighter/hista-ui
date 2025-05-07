1cf58b3e8e7f3f7af48b7213ed861eeb027c80b7
Benjamin Heller <benjamin.heller@mein.gmx>
2025-05-07 22:02:40 +0200

    ref: use const instead of namespace

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,15 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-export namespace url {
-    export const MEAL = '/meals'
-    export const CONDITION_EVENTS = '/condition-events'
-    export const STATISTICS = '/statistics'
-    export const NOTES = '/notes'
-    export const POLLENS = '/pollens'
-    export const STATUSES = '/statuses'
-    export const HEADACHES = '/headaches'
-    export const MANAGE_SYMPTOMS = '/manage-symptoms'
-}
+export const url = {
+    MEAL: '/meals',
+    CONDITION_EVENTS: '/condition-events',
+    STATISTICS: '/statistics',
+    NOTES: '/notes',
+    POLLENS: '/pollens',
+    STATUSES: '/statuses',
+    HEADACHES: '/headaches',
+    MANAGE_SYMPTOMS: '/manage-symptoms',
+} as const;
 
-export namespace mealConstants {
-    export const RAW = 'Roh'
-    export const COOKED = 'Gar'
-}
+export const mealConstants = {
+    RAW: 'Roh',
+    COOKED: 'Gar',
+} as const;
