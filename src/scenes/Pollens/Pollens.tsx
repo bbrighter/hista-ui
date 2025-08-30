dcda4e6b4b773bf6d00d2a3643b8b4666b9d05b3
Benjamin Heller <benjamin.heller@mein.gmx>
2025-08-30 10:22:04 +0200

    ref: add test for pollen

--- src/scenes/Pollens/Pollens.tsx
+++ src/scenes/Pollens/Pollens.tsx
@@ -1,32 +1,12 @@
-/// <reference types="vite-plugin-svgr/client" />
-import styled from '@emotion/styled'
 import Container from '@mui/material/Container'
-import Divider from '@mui/material/Divider'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import Paper from '@mui/material/Paper'
-import Skeleton from '@mui/material/Skeleton'
-import { Theme } from '@mui/material/styles'
-import SvgIcon from '@mui/material/SvgIcon';
-import Typography from '@mui/material/Typography'
-import useMediaQuery from '@mui/material/useMediaQuery';
 import { useEffect, useState } from 'react'
 
-import { Pollens } from '../../store/pollen/pollen'
 import useHista from '../../store/store'
-import AmbrosiaIcon from './ambrosia.svg?react'
-import BeifussIcon from './beifuss.svg?react'
-import BirkeIcon from './birke.svg?react'
-import ErleIcon from './erle.svg?react'
-import EscheIcon from './esche.svg?react'
-import GraeserIcon from './graeser.svg?react'
-import HaselIcon from './hasel.svg?react'
-import RoggenIcon from './roggen.svg?react'
+import PollenGrid from './components/PollenGrid'
 
 
-export default function Pollensview() {
+export default function Pollens() {
     const getPollens = useHista(state => state.getPollens)
-    const pollens = useHista(state => state.pollens)
     const pollensAreLoaded = useHista(state => state.pollensAreLoaded)
 
     const [isLoading, setIsLoading] = useState(false)
@@ -41,114 +21,7 @@ export default function Pollensview() {
 
     return (
         <Container sx={{ padding: 2 }}>
-            <PollenHeader />
-            <PollenGrid pollens={pollens} isLoading={isLoading} />
+            <PollenGrid loading={isLoading} />
         </Container >
-    )
-}
-
-const StyledDate = styled(Paper)`
-    width: max(20%, 100px);
-`
-
-function PollenGrid(props: {
-    pollens: Pollens,
-    isLoading: boolean
-}) {
-    type StyledPollenProps = {
-        intensity: number
-    }
-
-    const StyledPollen = styled.span<StyledPollenProps>`
-        background-color: ${props => intensityToColor(props.intensity)};
-        border-radius: 5px;
-        width: max(10%, 20px);      
-        height: 1.5rem;
-        margin-left: 2px;
-    `
-
-    const intensityToColor = (intensitiy: number): string => {
-        switch (intensitiy) {
-            case 1:
-                return 'darkgreen'
-            case 2:
-                return 'green'
-            case 3:
-                return 'greenyellow'
-            case 4:
-                return 'yellow'
-            case 5:
-                return 'orange'
-            case 6:
-                return 'red'
-            case 7:
-                return 'purple'
-            default:
-                return '#121212'
-        }
-    }
-
-    return (
-        <List>
-            {props.isLoading &&
-                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(p => (
-                    <Skeleton
-                        key={p}
-                        variant="rectangular"
-                        width={'calc(max(20%, 100px) + 8 * max(10%, 20px))'}
-                        height={'1.5rem'}
-                        sx={{ marginTop: 2 }}
-                    />))
-            }
-            {!props.isLoading && props.pollens.map((pollen, i) =>
-                <Typography key={i}>
-                    <ListItem >
-                        <StyledDate>{pollen.date.toLocaleDateString('de-DE')}</StyledDate>
-                        <StyledPollen intensity={pollen.ambrosia.intensity} />
-                        <StyledPollen intensity={pollen.beifuss.intensity} />
-                        <StyledPollen intensity={pollen.birke.intensity} />
-                        <StyledPollen intensity={pollen.erle.intensity} />
-                        <StyledPollen intensity={pollen.esche.intensity} />
-                        <StyledPollen intensity={pollen.graeser.intensity} />
-                        <StyledPollen intensity={pollen.hasel.intensity} />
-                        <StyledPollen intensity={pollen.roggen.intensity} />
-                    </ListItem>
-                </Typography>,
-            )}
-        </List>
-    )
-}
-
-function PollenHeader() {
-    return (
-        <Typography>
-            <ListItem>
-                <StyledDate></StyledDate>
-                <StyledTitle longTitle="Ambrosia" shortTitle={AmbrosiaIcon} />
-                <StyledTitle longTitle="Beifuss" shortTitle={BeifussIcon} />
-                <StyledTitle longTitle="Birke" shortTitle={BirkeIcon} />
-                <StyledTitle longTitle="Erle" shortTitle={ErleIcon} />
-                <StyledTitle longTitle="Esche" shortTitle={EscheIcon} />
-                <StyledTitle longTitle="Gräser" shortTitle={GraeserIcon} />
-                <StyledTitle longTitle="Hasel" shortTitle={HaselIcon} />
-                <StyledTitle longTitle="Roggen" shortTitle={RoggenIcon} />
-            </ListItem>
-            <Divider />
-        </Typography >
-    )
-}
-
-function StyledTitle(props: {
-    longTitle: string
-    shortTitle: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-}) {
-    const isXs = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
-
-    return (
-        <Paper sx={{ width: 'max(10%, 20px)' }}>
-            {!isXs && <SvgIcon component={props.shortTitle} inheritViewBox color='action' />}
-            {isXs && <Typography noWrap>{props.longTitle} </Typography>}
-        </Paper>
-
     )
 }
---
+++ src/scenes/Pollens/components/PollenGrid.tsx
@@ -0,0 +1,116 @@
+/// <reference types="vite-plugin-svgr/client" />
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { DataGrid, GridCellParams, gridClasses, GridColDef, GridColumnHeaderParams } from '@mui/x-data-grid';
+
+import { PollenIntensity } from '../../../store/pollen/pollen';
+import useHista from '../../../store/store';
+import AmbrosiaIcon from './ambrosia.svg?react'
+import BeifussIcon from './beifuss.svg?react'
+import BirkeIcon from './birke.svg?react'
+import ErleIcon from './erle.svg?react'
+import EscheIcon from './esche.svg?react'
+import GraeserIcon from './graeser.svg?react'
+import HaselIcon from './hasel.svg?react'
+import RoggenIcon from './roggen.svg?react'
+
+export default function PollenGrid(props: { loading: boolean }) {
+    const isSmall = useMediaQuery('(max-width:600px)');
+
+    const iconMap: Record<string, React.ElementType> = {
+        ambrosia: AmbrosiaIcon,
+        beifuss: BeifussIcon,
+        birke: BirkeIcon,
+        erle: ErleIcon,
+        esche: EscheIcon,
+        graeser: GraeserIcon,
+        hasel: HaselIcon,
+        roggen: RoggenIcon,
+    };
+
+    const pollens = useHista(state => state.pollens)
+    const columns: Array<GridColDef> = [
+        { field: 'date', headerName: 'Datum', valueFormatter: (p: Date) => p.toLocaleDateString('de-DE') },
+        { field: 'ambrosia', headerName: 'Ambrosia', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <AmbrosiaIcon width={16} color='white' /> : p.colDef.headerName) },
+        { field: 'beifuss', headerName: 'Beifuss', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <BeifussIcon width={16} /> : p.colDef.headerName) },
+        { field: 'birke', headerName: 'Birke', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <BirkeIcon width={16} /> : p.colDef.headerName) },
+        { field: 'erle', headerName: 'Erle', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <ErleIcon width={16} /> : p.colDef.headerName) },
+        { field: 'esche', headerName: 'Esche', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <EscheIcon width={16} /> : p.colDef.headerName) },
+        { field: 'graeser', headerName: 'Gräser', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <GraeserIcon width={16} /> : p.colDef.headerName) },
+        { field: 'hasel', headerName: 'Hasel', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <HaselIcon width={16} /> : p.colDef.headerName) },
+        { field: 'roggen', headerName: 'Roggen', renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <RoggenIcon width={16} color='white' /> : p.colDef.headerName) },
+    ].map(col => {
+        if (col.field == 'date') return col
+
+        const Icon = iconMap[col.field]
+        return {
+            ...col,
+            valueFormatter: isSmall ? () => '' : (p: PollenIntensity) => p.intensityString,
+            flex: 1,
+            renderHeader: (p: GridColumnHeaderParams) => (isSmall ? <Icon width={16} fill='currentColor' /> : p.colDef.headerName),
+        }
+    })
+
+    const rows = pollens.map(p => ({
+        id: p.date.toISOString(),
+        date: p.date,
+        ambrosia: p.ambrosia,
+        beifuss: p.beifuss,
+        birke: p.birke,
+        erle: p.erle,
+        esche: p.esche,
+        graeser: p.graeser,
+        hasel: p.hasel,
+        roggen: p.roggen,
+    }))
+    return (
+        <Box
+            sx={{
+                height: 300,
+                width: '100%',
+                [`.${gridClasses.cell}.level-1`]: {
+                    backgroundColor: 'darkgreen',
+                    color: 'black',
+                },
+                [`.${gridClasses.cell}.level-2`]: {
+                    backgroundColor: 'green',
+                    color: 'black',
+                },
+                [`.${gridClasses.cell}.level-3`]: {
+                    backgroundColor: 'greenyellow',
+                    color: 'black',
+                },
+                [`.${gridClasses.cell}.level-4`]: {
+                    backgroundColor: 'yellow',
+                    color: 'black',
+                },
+                [`.${gridClasses.cell}.level-5`]: {
+                    backgroundColor: 'orange',
+                    color: 'white',
+                },
+                [`.${gridClasses.cell}.level-6`]: {
+                    backgroundColor: 'red',
+                    color: 'white',
+                },
+                [`.${gridClasses.cell}.level-7`]: {
+                    backgroundColor: 'purple',
+                    color: 'white',
+                },
+            }}
+        >
+            <DataGrid
+                loading={props.loading}
+                rows={rows}
+                columns={columns}
+                getCellClassName={(params: GridCellParams<unknown, PollenIntensity, number>) => {
+                    if (params.field === 'date') {
+                        return '';
+                    }
+                    return `level-${params.value.intensity}`;
+                }}
+                disableColumnMenu
+                disableColumnSorting
+            />
+        </Box>
+    );
+}
