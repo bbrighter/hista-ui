b1628d9a7584d6234e87833d6b229e11813cfcc6
Benjamin Heller <benjamin.heller@mein.gmx>
2025-09-01 20:05:40 +0200

    ref: Test headache

---
+++ src/scenes/Headache/Headache.test.tsx
@@ -0,0 +1,150 @@
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import useHista from '../../store/store';
+import Headache from './Headache';
+
+vi.mock('../../store/store', { spy: true })
+
+const getTagByText = (text: string): HTMLElement => {
+    const tagText = screen.getByText(text)
+    return tagText.closest('div')
+}
+
+const isTagActive = (text: string): boolean => {
+    const tag = getTagByText(text)
+    return tag.className.includes('colorPrimary')
+}
+
+describe('A headache can be edited and displayed', () => {
+    beforeEach(() => {
+        vi.resetAllMocks()
+    })
+    it('Change the date', { skip: true }, async () => {
+
+    })
+
+    it('Change severity', async () => {
+        const patchHeadacheSeverity = vi.spyOn(useHista.getState(), 'patchHeadacheSeverity')
+        render(<MemoryRouter initialEntries={['/headaches/1']}>
+            <Routes>
+                <Route path="/headaches/:id" element={<Headache />} />
+            </Routes>
+        </MemoryRouter>)
+
+        const symptom = await screen.findByText('Schwere')
+        const listItem = symptom.closest('div')
+        const slider = within(listItem).getByRole('slider')
+        expect(slider.ariaValueNow).toBe('3')
+
+        fireEvent.change(slider, { target: { value: '1' } })
+        expect(slider.ariaValueNow).toBe('1')
+        expect(screen.getByTestId('slider-icon')).toHaveStyle({ backgroundColor: 'rgb(51,255,0)' })
+        await waitFor(() => {
+            expect(patchHeadacheSeverity).toHaveBeenCalled()
+        })
+
+    })
+
+    it('Change position', async () => {
+        render(<MemoryRouter initialEntries={['/headaches/1']}>
+            <Routes>
+                <Route path="/headaches/:id" element={<Headache />} />
+            </Routes>
+        </MemoryRouter>)
+
+        const expectedTags = [
+            { name: 'Links', active: true },
+            { name: 'Rechts', active: true },
+            { name: 'Oben', active: false },
+            { name: 'Hinterkopf', active: false },
+            { name: 'Seite', active: false },
+            { name: 'Schläfe', active: false },
+            { name: 'Stirn', active: false },
+            { name: 'Ohr', active: false },
+            { name: 'Nacken', active: false },
+        ]
+        await waitFor(() => {
+            expectedTags.forEach(t => {
+                expect(getTagByText(t.name)).toBeInTheDocument()
+                expect(isTagActive(t.name)).toBe(t.active)
+            })
+        })
+
+        const upTag = getTagByText('Oben')
+        await userEvent.click(upTag)
+        expect(isTagActive('Oben')).toBeTruthy()
+
+        const leftTag = getTagByText('Links')
+        await userEvent.click(leftTag)
+        expect(isTagActive('Links')).toBeFalsy()
+    })
+
+    it('Change type', async () => {
+        const patchHeadacheTypes = vi.spyOn(useHista.getState(), 'patchHeadacheTypes')
+        render(<MemoryRouter initialEntries={['/headaches/1']}>
+            <Routes>
+                <Route path="/headaches/:id" element={<Headache />} />
+            </Routes>
+        </MemoryRouter>)
+
+        const expectedTags = [
+            { name: 'Pulsierend-pochend', active: false },
+            { name: 'Dumpf-drückend', active: false },
+            { name: 'Stechend', active: true },
+        ]
+        await waitFor(() => {
+            expectedTags.forEach(t => {
+                expect(getTagByText(t.name)).toBeInTheDocument()
+                expect(isTagActive(t.name)).toBe(t.active)
+            })
+        })
+
+        const stabbingTag = getTagByText('Stechend')
+        await userEvent.click(stabbingTag)
+        expect(patchHeadacheTypes).toHaveBeenCalled()
+        expect(isTagActive('Stechend')).toBeFalsy()
+    })
+
+    it('Change symptoms', async () => {
+        render(<MemoryRouter initialEntries={['/headaches/1']}>
+            <Routes>
+                <Route path="/headaches/:id" element={<Headache />} />
+            </Routes>
+        </MemoryRouter>)
+
+        await waitFor(() => {
+            expect(isTagActive('Übelkeit')).toBeTruthy()
+            expect(isTagActive('Schwindel')).toBeFalsy()
+        })
+        const dizzinessTag = getTagByText('Schwindel')
+        await userEvent.click(dizzinessTag)
+        expect(isTagActive('Schwindel')).toBeTruthy()
+
+        const nauseaTag = getTagByText('Übelkeit')
+        await userEvent.click(nauseaTag)
+        expect(isTagActive('Übelkeit')).toBeFalsy()
+    })
+
+    it('Change description', async () => {
+        // const patchHeadacheDescription = vi.spyOn(useHista.getState(), 'patchHeadacheDescription')
+        render(<MemoryRouter initialEntries={['/headaches/1']}>
+            <Routes>
+                <Route path="/headaches/:id" element={<Headache />} />
+            </Routes>
+        </MemoryRouter>)
+
+        const description = await screen.findByText('description')
+        expect(description).toBeInTheDocument()
+
+        await userEvent.type(description, '2')
+        expect(screen.getByText('description2')).toBeInTheDocument()
+        // Timeout is too long!
+        // await waitFor(() => {
+        //     expect(patchHeadacheDescription).toHaveBeenCalled()
+        // }, { timeout: 5000 })
+
+    })
+})
--- src/scenes/Headache/Headache.tsx
+++ src/scenes/Headache/Headache.tsx
@@ -46,7 +46,9 @@ export default function Headache() {
                 justifyContent: 'center',
                 fontSize: '1.2rem',
                 fontWeight: 'bold',
-            }}>{value}</Typography>
+            }}
+                data-testid='slider-icon'
+            >{value}</Typography>
 
         )
     }
