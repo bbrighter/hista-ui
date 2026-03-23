2af61cbc09b711149a0b4028eb4278e3d6238ce3
Benjamin Heller <benjamin.heller@mein.gmx>
2026-03-23 20:41:19 +0100

    fix: fullscreen modal

--- src/scenes/Meal/components/ShowNutritionChart.tsx
+++ src/scenes/Meal/components/ShowNutritionChart.tsx
@@ -1,11 +1,17 @@
+import CloseIcon from "@mui/icons-material/Close";
 import PieChartIcon from "@mui/icons-material/PieChart";
+import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box"
 import CircularProgress from "@mui/material/CircularProgress";
 import Dialog from "@mui/material/Dialog"
 import DialogContent from "@mui/material/DialogContent"
 import IconButton from "@mui/material/IconButton"
+import Slide from "@mui/material/Slide";
+import Toolbar from "@mui/material/Toolbar";
+import { TransitionProps } from "@mui/material/transitions";
 import dayjs from "dayjs";
 import { useEffect, useMemo, useState } from "react"
+import React from "react";
 
 import { statisticsService } from "../../../store"
 import useHista from "../../../store/store";
@@ -35,22 +41,43 @@ export const ShowNutritionChart = () => {
     <Box>
       <IconButton onClick={() => setOpen(true)}><PieChartIcon/></IconButton>
       <Dialog 
-        open={open} onClose={() => setOpen(false)}
-        fullWidth
-        maxWidth="sm"
-        sx={{
-          minHeight: 200,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+        open={open} 
+        onClose={() => setOpen(false)}
+        fullScreen
+        slots={{
+          transition: Transition,
         }}
       >
-        <DialogContent> 
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={() => setOpen(false)}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <DialogContent
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}> 
           {isLoading ? <CircularProgress/> : <NutritionChart nutrition={nutrition}/>}
         </DialogContent>
       </Dialog>
     </Box>
   )
-
-
 }
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<unknown>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
