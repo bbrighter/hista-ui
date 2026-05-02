7ed9a055e6981b1c5bb2c571a58a591bc4057826
Benjamin Heller <benjamin.heller@mein.gmx>
2026-05-02 09:25:45 +0200

    fix: Ensure initial loading

--- src/actions/__test__/status.service.test.ts
+++ src/actions/__test__/status.service.test.ts
@@ -1,8 +1,10 @@
 import dayjs from "dayjs";
 import { beforeEach, describe, expect, it } from "vitest";
 
+import { hista } from "../../api/generatedApi";
 import useHista from "../../store/store";
 import { actions } from "..";
+import { respToStatuses } from "../status.actions";
 
 describe("status service", () => {
 
@@ -58,5 +60,31 @@ describe("status service", () => {
     expect(status.eveningFitness).toBe(2)
     expect(status.morningFitness).toBe(3)
 
+  })
+})
+
+
+describe("response to status", () => {
+  it("response to status", () => {
+    const resp = { statuses: [
+      { id: 1, morningSleep: 2, morningFitness: null, date: "2026-05-01T19:18:25.227+02:00" },
+    ] } satisfies hista.StatusListResponse
+    
+    const statuses = respToStatuses(resp)
+    expect(statuses).toHaveLength(1)
+    const status = statuses[0]
+    expect(status.id).toBe(1)
+    expect(status.date.year()).toBe(2026)
+    expect(status.date.month()).toBe(4)
+    expect(status.date.date()).toBe(1)
+    expect(status.eveningFitness).toBeNull()
+    expect(status.morningFitness).toBeNull()
+    expect(status.morningSleep).toBe(2)
+  })
+
+  it("empty response handled", () => {
+    const resp = {} as hista.StatusListResponse
+    const status = respToStatuses(resp)
+    expect(status).toHaveLength(0)
   })
 })
