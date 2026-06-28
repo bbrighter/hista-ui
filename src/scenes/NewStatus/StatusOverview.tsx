96e0dfbedf892a831ca7b2fb41a0035cb6d7369b
Benjamin Heller <benjamin.heller@mein.gmx>
2026-06-28 20:14:24 +0200

    feat: First draft for evaluation

---
+++ src/scenes/NewStatus/StatusOverview.tsx
@@ -0,0 +1,23 @@
+import Container from "@mui/material/Container";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import { useState } from "react";
+import useHista from "../../store/store";
+import { StatusEvaluation } from "./StatusEvaluation";
+import { StatusList } from "./StatusList";
+
+export const StatusOverview = () => {
+	const statuses = useHista((state) => state.statuses);
+	const [tab, setTab] = useState(0);
+
+	return (
+		<Container>
+			<Tabs value={tab} onChange={(_e, v) => setTab(v)}>
+				<Tab label="Status" value={0} />
+				<Tab label="Auswertung" value={1} />
+			</Tabs>
+			{tab === 0 && <StatusList />}
+			{tab === 1 && <StatusEvaluation statuses={statuses} />}
+		</Container>
+	);
+};
