f47070662ac54f5bd3e3675fe8ca298bb9678fab
Benjamin Heller <benjamin.heller@mein.gmx>
2026-08-11 20:42:21 +0200

    ref: try updating workflows

--- knip.config.ts
+++ knip.config.ts
@@ -4,6 +4,7 @@ const config: KnipConfig = {
 	ignore: ["src/api/generatedApi.ts", "scripts/*"],
 	ignoreBinaries: [
 		"dot", // Needed to visualize results from dependency-cruiser
+		"tsc", // Currently, two TS versions are in use
 	],
 	ignoreDependencies: [
 		"source-map", // Needed for the script evaluateMinifiedBuild.js
