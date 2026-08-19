142ab76c0bd24db44f57e4933accda2e0ec4a143
Benjamin Heller <benjamin.heller@mein.gmx>
2026-08-19 21:02:34 +0200

    fix: Fix test

--- src/scenes/Statistics/Headaches/HeadachesDiary.test.tsx
+++ src/scenes/Statistics/Headaches/HeadachesDiary.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-
+import { createHeadache } from "@/__tests__/fixtures/headache";
+import { getHeadacheListHandler } from "@/__tests__/mocks/headacheHandlers";
+import { server } from "@/__tests__/setupTest";
 import { HeadacheDiary } from "./HeadachesDiary";
 
 describe("headache grid is rendered and headaches can be downloaded", () => {
 	it("everything is rendered", async () => {
+		server.use(
+			getHeadacheListHandler(createHeadache({ description: "description" })),
+		);
 		render(<HeadacheDiary />);
 
 		expect(
